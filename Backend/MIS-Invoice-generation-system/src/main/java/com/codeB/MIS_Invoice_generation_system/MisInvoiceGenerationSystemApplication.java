caf74131ad857e18be6dd1f085eb102b7ec509ef
pandit2512 <84197105+pandit2512@users.noreply.github.com>
2025-04-09 14:11:03 +0530

    initialised the Project

---
+++ Backend/MIS-Invoice-generation-system/src/main/java/com/codeB/MIS_Invoice_generation_system/MisInvoiceGenerationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.codeB.MIS_Invoice_generation_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MisInvoiceGenerationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MisInvoiceGenerationSystemApplication.class, args);
+	}
+
+}
